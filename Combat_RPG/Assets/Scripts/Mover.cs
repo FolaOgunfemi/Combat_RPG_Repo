01e56cef4b328bee090ab2d6ca998de98566983f
folaranmi_ogunfemi <folaranmi_ogunfemi@comcast.com>
2019-04-05 16:18:29 -0400

    basic terrain and navmesh agent

---
+++ Combat_RPG/Assets/Scripts/Mover.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField]
+    private Transform m_Target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        NavMeshAgent nav = GetComponent<NavMeshAgent>();
+
+        nav.SetDestination(m_Target.position);
+
+
+    }
+}
