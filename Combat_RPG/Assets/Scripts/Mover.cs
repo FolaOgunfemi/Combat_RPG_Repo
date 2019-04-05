ddd7734040b87b6687c1a3b161010b0344bab727
folaranmi_ogunfemi <folaranmi_ogunfemi@comcast.com>
2019-04-05 16:55:46 -0400

    raycast camera pointer

--- Combat_RPG/Assets/Scripts/Mover.cs
+++ Combat_RPG/Assets/Scripts/Mover.cs
@@ -8,6 +8,8 @@ public class Mover : MonoBehaviour
     [SerializeField]
     private Transform m_Target;
 
+
+    Ray m_LastRay;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,6 +24,18 @@ public class Mover : MonoBehaviour
 
         nav.SetDestination(m_Target.position);
 
+        RayCastPointer();
 
+    }
+
+    private void RayCastPointer()
+    {
+        //when we left click, we will do the following
+        if (Input.GetMouseButtonDown(0))
+        {
+            m_LastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
+        //starting point is origin... we multiply by 100 to make the ray go much further than the destination point, that way we can see it well
+        Debug.DrawRay(m_LastRay.origin, m_LastRay.direction * 100);
     }
 }
